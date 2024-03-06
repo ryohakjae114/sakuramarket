94f6d51703f6ba8d6147c78330deb13600668cc3
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-03-06 14:15:34 +0900

    ユーザの認証わまり、管理者のログアウト機能のテストを追加

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,16 @@
 class AdminsController < ActionController::Base
   layout 'admin'
   before_action :authenticate_admin!
+
+  private
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+    admin_root_path
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_admin_user_session_path
+  end
 end
--- spec/system/admins/authentication_spec.rb
+++ spec/system/admins/authentication_spec.rb
@@ -10,7 +10,19 @@ RSpec.describe '管理者認証' do
     fill_in 'パスワード', with: '12341234'
     expect do
       find('input[type="submit"]').click
+      expect(current_path).to eq admins_root_path
       expect('page').to have_content 'ログインしました。'
+    end
+  end
+
+  it 'ログアウトできること' do
+    sign_in admin
+    visit admins_root_path
+    expect do
+      click 'ログアウト'
+      click 'OK'
+      visit admins_food_path
+      expect('page').to eq new_admin_session_path
     end
   end
 end
