0228afc0f41400feb226c770bd9b078a71313a30
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-03-15 13:23:22 +0900

    rubocop修正

--- app/models/purchase_detail.rb
+++ app/models/purchase_detail.rb
@@ -5,11 +5,6 @@ class PurchaseDetail < ApplicationRecord
   validates :number, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1}
 
   def total_price_with_tax
-    self.food.price_with_tax * self.number
-  end
-
-  def add_number!(number)
-    self.number += number
-    self.save!
+    food.price_with_tax * number
   end
 end
